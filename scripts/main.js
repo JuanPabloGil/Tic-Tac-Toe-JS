f91d9f1f32b48c7abc0baf92591738dc1f5ad9ed
Juan Pablo <jpablomgil@gmail.com>
2020-04-17 15:30:15 -0500

    Validate players input

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,27 @@
 
+function validatePlayers(player1, player2) {
+  if (player1.length > 3 && player2.length > 3) {
+    return true
+  }
+}
+
 const button = document.getElementById('button');
 
 button.addEventListener('click', () => {
   const form = document.getElementById('form');
 
-  const playerOne =form.playerOne.value;
-  const playerTwo =form.playerTwo.value;
+  const p1 = form.playerOne;
+  const p2 = form.playerTwo;
 
-  //if (validatePlayers(playerOne,playerTwo) {
-  gameController.players = gameController.get_players(playerOne, playerTwo);
-  //}
+  if (validatePlayers(p1.value, p2.value)) {
+    document.getElementById('error_message').textContent = "";
+    p1.value = "";
+    p2.value = "";
+    gameController.players = gameController.get_players(p1.value, p2.value);
+  }else{
+    document.getElementById('error_message').textContent = "The playe's name must have more than 3 chars ";
+  }
+
 });
 
 
