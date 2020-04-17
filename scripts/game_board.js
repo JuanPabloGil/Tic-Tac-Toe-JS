d3e979a06e7bc6ea9fb3cfb6b1ef9970cd92b4b9
Ivan <fivanunam@gmail.com>
2020-04-17 17:22:35 -0500

    add there_is_place and there_is_winner methods

--- scripts/game_board.js
+++ scripts/game_board.js
@@ -1,16 +1,42 @@
 const gameBoard = (function() {
-  const LINE_TO_WIN = [[1, 2, 3],
-                      [4, 5, 6],
-                      [7, 8, 9],
+  const LINES_TO_WIN = [[0, 1, 2],
+                      [3, 4, 5],
+                      [6, 7, 8],
+                      [0, 3, 6],
                       [1, 4, 7],
                       [2, 5, 8],
-                      [3, 6, 9],
-                      [1, 5, 9],
-                      [3, 5, 7]];
+                      [0, 4, 8],
+                      [2, 4, 6]];
+
+  const linesToWin = () => {
+    return LINES_TO_WIN;
+  };
   
-  let arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let arr = ['✘', '✘', '✘', '4', '5', '6', '7', '8', '9'];
+  //let arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  const there_is_place = function() {
+    let response = false;
+    this.arr.forEach((item) => {
+      if (item != '✘' && item != '●') {
+        response = true;
+      }
+    });
+    return response;
+  };
+
+  const there_is_winner = function() {
+    let response = false;
+    linesToWin().forEach((line) => {
+      if (this.arr[line[0]] == this.arr[line[1]] && this.arr[line[1]] == this.arr[line[2]])
+        response = true;
+    }, this);
+    return response;
+  };
 
   return {
-    arr
+    arr,
+    there_is_place,
+    there_is_winner
   }
 })();
