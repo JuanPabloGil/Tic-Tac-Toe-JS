7c82706e3b35a7561f413b3259d53e76febf2a8a
Ivan <fivanunam@gmail.com>
2020-04-21 11:04:10 -0500

    change symbol display for name player

--- scripts/game_controller.js
+++ scripts/game_controller.js
@@ -13,18 +13,18 @@ const gameController = (function(doc) {
   const get_players = (playerOne, playerTwo) => {
     return [Player(
       playerOne,
-      '✘'
+      '●'
     ),
     Player(
       playerTwo,
-      '●'
+      '✘'
     )];
   }
 
   let current_player = 0 ;
 
   const display_current_turn = function(current_player) {
-    document.getElementById('cPlayer').textContent =  "Player's turn: " + this.players[current_player].symbol;
+    doc.getElementById('cPlayer').textContent =  "Player's turn: " + this.players[current_player].name;
   }
 
   let against_computer = true;
