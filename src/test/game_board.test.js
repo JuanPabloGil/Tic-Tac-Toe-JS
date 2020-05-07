6910bca6bead501b8f5c97a8ee0edeabf0108aec
Juan Pablo <jpablomgil@gmail.com>
2020-05-07 16:50:53 -0500

    Adding test cases for  addMove

--- src/test/game_board.test.js
+++ src/test/game_board.test.js
@@ -4,17 +4,36 @@ test('Cheking if there is a place on the board case false ', () => {
   expect(gameBoard.thereIsPlace()).toBe(true);
 });
 
-// describe('matching cities to foods', () => {
-//   // Applies only to tests in this describe block
-//   beforeEach(() => {
-//     return initializeFoodDatabase();
-//   });
 
-//   test('Vienna <3 sausage', () => {
-//     expect(isValidCityFoodPair('Vienna', 'Wiener Schnitzel')).toBe(true);
-//   });
+beforeEach(() => {
+    gameBoard.arr  = ['', '', '', '', '', '', '', '', ''];
+});
 
-//   test('San Juan <3 plantains', () => {
-//     expect(isValidCityFoodPair('San Juan', 'Mofongo')).toBe(true);
-//   });
-// });
+describe('#add_move', () => {
+
+  test('modify a place  that is availabe', () => {
+    gameBoard.addMove('1', '✘');
+    expect(gameBoard.arr[0]).toBe('✘');
+  });
+
+  test('choose a place that is occupied', () => {
+    gameBoard.arr  = ['✘', '', '', '', '', '', '', '', ''];
+    expect(gameBoard.addMove('1', '●')).toBe(false);
+  });
+
+  test('didn\'t modify a place that is occupied', () => {
+    gameBoard.addMove('1', '✘');
+    gameBoard.addMove('1', '●');
+    expect(gameBoard.arr[0]).toBe('✘');
+  });
+
+  test('Check if the place hhas been chosen as string ', () => {
+    expect(gameBoard.addMove(1, '✘')).toBe(false);
+  });
+
+  test('choose a place out of the range', () => {
+    expect(gameBoard.addMove('12', '✘')).toBe(false);
+  });
+
+
+});
