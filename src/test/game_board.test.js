9690efe1d8ced8ae8d1883fa5a8b1c716de3ce8b
Ivan <fivanunam@gmail.com>
2020-05-07 16:20:54 -0500

    add gameBoard module prefix

--- src/test/game_board.test.js
+++ src/test/game_board.test.js
@@ -1,5 +1,20 @@
 import gameBoard from '../scripts/game_board';
 
 test('Cheking if there is a place on the board case false ', () => {
-  expect(thereIsPlace()).toBe(false);
+  expect(gameBoard.thereIsPlace()).toBe(true);
 });
+
+// describe('matching cities to foods', () => {
+//   // Applies only to tests in this describe block
+//   beforeEach(() => {
+//     return initializeFoodDatabase();
+//   });
+
+//   test('Vienna <3 sausage', () => {
+//     expect(isValidCityFoodPair('Vienna', 'Wiener Schnitzel')).toBe(true);
+//   });
+
+//   test('San Juan <3 plantains', () => {
+//     expect(isValidCityFoodPair('San Juan', 'Mofongo')).toBe(true);
+//   });
+// });
